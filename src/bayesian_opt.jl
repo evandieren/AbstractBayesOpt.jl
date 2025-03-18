e1c164d0a2252017bc1572161af84e7656eadbcd
Eliott Van Dieren <vandieren.eliott@gmail.com>
2025-03-18 12:15:06 +0100

    adding noise

--- src/bayesian_opt.jl
+++ src/bayesian_opt.jl
@@ -102,7 +102,7 @@ function optimize(p::BOProblem)
         end
         x_cand = optimize_acquisition!(p.acqf,p.gp,p.domain)
         println("New point acquired: ",x_cand)
-        y_cand = p.f(x_cand)
+        y_cand = p.f(x_cand) + p.noise^2*randn()
         println("New value probed: ",y_cand)
         i +=1
         p = update!(p, x_cand, y_cand, i)
