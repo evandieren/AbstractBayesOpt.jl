a7e12b391b97cb1d96f2b474ce15a4de274dec9d
Eliott Van Dieren <vandieren.eliott@gmail.com>
2025-06-05 18:42:41 +0200

    Adding UCB

---
+++ src/acquisition/UpperConfidenceBound.jl
@@ -0,0 +1,13 @@
+struct UpperConfidenceBound <: AbstractAcquisition
+     β::Float64  # exploration-exploitation balance parameter
+end
+
+function (ucb::UpperConfidenceBound)(surrogate::AbstractSurrogate, x)
+    μ = posterior_mean(surrogate, x)
+    σ² = posterior_var(surrogate, x)
+    return μ - ucb.β*σ²
+end
+
+function update!(acqf::UpperConfidenceBound,ys::AbstractVector, surrogate::AbstractSurrogate)
+    acqf
+end
