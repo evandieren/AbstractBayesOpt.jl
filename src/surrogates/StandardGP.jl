f6254beff671c1e78ea25ce4a5460c527a3c3771
Eliott Van Dieren <vandieren.eliott@gmail.com>
2025-08-21 13:25:59 +0200

    removing prints

--- src/surrogates/StandardGP.jl
+++ src/surrogates/StandardGP.jl
@@ -36,15 +36,15 @@ function nlml(mod::StandardGP,params,kernel,x,y;mean=ZeroMean())
 
     # Kernel with current parameters
     k = scale * (kernel ∘ ScaleTransform(1/ℓ))
-    println("creation time of standardgp")
-    @time gp = StandardGP(k, mod.noise_var,mean=mean) # Use fixed noise here, or optimize σ² too
+    #println("creation time of standardgp")
+    gp = StandardGP(k, mod.noise_var,mean=mean) # Use fixed noise here, or optimize σ² too
 
     # Evaluate GP at training points with noise, creates a FiniteGP
-    println("finite gpx time")
-    @time gpx = gp.gp(x,mod.noise_var)
+    #println("finite gpx time")
+    gpx = gp.gp(x,mod.noise_var)
 
-    println("logpdf")
-    @time -AbstractGPs.logpdf(gpx, y)
+    #println("logpdf")
+    -AbstractGPs.logpdf(gpx, y)
 end
 
 function standardize_y(mod::StandardGP,y_train::AbstractVector)
