9cfd020ab0c02c4d74ac55110411b2f608645082
Eliott Van Dieren <vandieren.eliott@gmail.com>
2025-09-17 11:48:37 +0200

    adding more information about the abstraction (docs and code)

--- docs/src/index.md
+++ docs/src/index.md
@@ -16,13 +16,67 @@ The library uses **Bayesian Optimisation (BO)** to iteratively propose evaluatio
 
 ## Abstract Interfaces
 
+We currently have three main abstract interfaces that work with our BO loop:
+
 - `AbstractAcquisition`: Interface to implement for an acquisition function to be used in `AbstractBayesOpt`.
 - `AbstractSurrogate`: Interface to implement for a surrogate to be used in `AbstractBayesOpt`.
 - `AbstractDomain`: Interface to implement for the optimisation domain to be used in `AbstractBayesOpt`.
 
-These define the abstract types and required methods for surrogates, acquisitions, and domains. The BO loop only relies on these abstract classes.
+AbstractBayesOpt.jl defines the core abstractions for building Bayesian optimization
+algorithms. To add a new surrogate model, acquisition function, or domain, implement
+the following interfaces:
 
-## What is currently implemented
+---
+
+### Surrogates
+
+Subtype [`AbstractSurrogate`](@ref) and implement:
+
+- `update(model::AbstractSurrogate, xs::AbstractVector, ys::AbstractVector)`:  
+  Update the surrogate with new data `(xs, ys)`.
+
+- `posterior_mean(model::AbstractSurrogate, x::AbstractVector)`:  
+  Return the posterior mean at point `x`.
+
+- `posterior_var(model::AbstractSurrogate, x::AbstractVector)`:  
+  Return the posterior variance at point `x`.
+
+- `nlml(model::AbstractSurrogate, params::AbstractVector, xs::AbstractVector, ys::AbstractVector)`:  
+  Compute the negative log marginal likelihood given hyperparameters and data.
+
+---
+
+### Acquisition Functions
+
+Subtype [`AbstractAcquisition`](@ref) and implement:
+
+- `(acq::AbstractAcquisition)(model::AbstractSurrogate, x, x_buf=nothing)`:  
+  Evaluate the acquisition function at `x`.  
+  Optionally use a buffer `x_buf` for caching.
+
+- `update(acq::AbstractAcquisition, ys::AbstractVector, model::AbstractSurrogate)`:  
+  Update acquisition state given new observations.
+
+- `Base.copy(acq::AbstractAcquisition)`:  
+  Return a copy of the acquisition function.
+
+---
+
+### Domains
+
+Subtype [`AbstractDomain`](@ref) and implement:
+
+Concrete implementations should subtype this and define the necessary properties:
+- `lower`: The lower bounds of the domain.
+- `upper`: The upper bounds of the domain.
+
+as well as creating its constructor.
+---
+
+Concrete implementations may add additional methods as needed, but these are the
+minimum required for compatibility with the Bayesian optimization loop.
+
+## What abstract subtypes are currently implemented in AbstractBayesOpt
 
 ### Acquisition functions
 - `ExpectedImprovement`: Standard expected improvement acquisition function for balancing exploration and exploitation.
--- src/abstract.jl
+++ src/abstract.jl
@@ -3,20 +3,60 @@
 
 An abstract type for surrogate models and acquisition functions used in Bayesian optimization.
 
-Use abstraction to cover the future addition of gradients, and keep the types used in AbstractBayesOpt.jl 
-seperated from their implementation (e.g. using AbstractGP.jl and kernelfunctions.jl) in folders acquisition/ and surrogates/
+Use abstraction to keep the types used in AbstractBayesOpt.jl 
+separated from their implementation (e.g. using AbstractGP.jl and KernelFunctions.jl) in folders acquisition/ and surrogates/
 """
 # AbstractBayesOpt.jl: Abstract types and update/optimize functions
 
-# Abstract Acquisition function
+"""
+    AbstractSurrogate
+
+Abstract type for surrogate models used in Bayesian optimization.
+
+Concrete implementation should subtype this and implement the following methods:
+- `update(model::AbstractSurrogate, xs::AbstractVector, ys::AbstractVector)`: 
+    Update the surrogate model with new data points `xs` and corresponding observations `ys`.
+- `posterior_mean(surrogate::AbstractSurrogate, x::AbstractVector)`: 
+    Compute the posterior mean of the surrogate model at point `x`.
+- `posterior_var(surrogate::AbstractSurrogate, x::AbstractVector)`: 
+    Compute the posterior variance of the surrogate model at point `x`.
+- `nlml(surrogate::AbstractSurrogate, params::AbstractVector, xs::AbstractVector, ys::AbstractVector)`: 
+    Compute the negative log marginal likelihood of the surrogate model given hyperparameters `params`, input data `xs`, and observations `ys`.
+
+
+Other methods can be added as needed depending on the use case.
+"""
+abstract type AbstractSurrogate end
+
+
+"""
+    AbstractAcquisition
+
+Abstract type for acquisition functions used in Bayesian optimization.
+
+Concrete implementation should subtype this and implement the following methods:
+- `(acq::AbstractAcquisition)(surrogate::AbstractSurrogate, x, x_buf=nothing)`: 
+    Evaluate the acquisition function at point `x` using the surrogate model. Optionally, a buffer `x_buf` can be provided for caching purposes.
+- `update(acq::AbstractAcquisition, ys::AbstractVector, model::AbstractSurrogate)`: 
+    Update the acquisition function with new observations `ys` and the current surrogate model.
+- `Base.copy(acq::AbstractAcquisition)`: 
+    Create a copy of the acquisition function.
+"""
 abstract type AbstractAcquisition end
-abstract type AbstractSurrogate <: AbstractGPs.AbstractGP end
+
+
+
+""" 
+    AbstractDomain
+
+An abstract type for defining the domain over which the optimization is performed.
+
+Concrete implementations should subtype this and define the necessary properties:
+- `lower`: The lower bounds of the domain.
+- `upper`: The upper bounds of the domain.
+
+as well as creating its constructor.
+
+Other methods can be added as needed depending on the use case.
+"""
 abstract type AbstractDomain end
-
-# Update functions for models, acquisition functions, and BO structure to implement 
-function update(model::AbstractSurrogate, xs::AbstractVector, ys::AbstractVector) end
-
-function update(acq::AbstractAcquisition, ys::AbstractVector, model::AbstractSurrogate) end
-
-# Optimization functions for acquisition functions
-function optimize_acquisition end
