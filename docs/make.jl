fd47ffb42cf02db92823a1712416b6416ec129a9
Eliott Van Dieren <vandieren.eliott@gmail.com>
2025-09-22 22:18:43 +0200

    Some duplicate lines

--- docs/make.jl
+++ docs/make.jl
@@ -6,10 +6,8 @@ LITERATE_INPUT = joinpath(@__DIR__, "literate")
 LITERATE_OUTPUT = joinpath(@__DIR__, "src")
 
 for dir_path in filter(isdir, readdir(joinpath(@__DIR__, "literate"), join = true))
-for dir_path in filter(isdir, readdir(joinpath(@__DIR__, "literate"), join = true))
     dirname = basename(dir_path)
 
-    for (root, _, files) in walkdir(dir_path), file in files
     for (root, _, files) in walkdir(dir_path), file in files
         # ignore non julia files
         splitext(file)[2] == ".jl" || continue
@@ -27,10 +25,6 @@ makedocs(;
     modules = [AbstractBayesOpt],
     checkdocs = :none,
     pages = [
-    sitename = "AbstractBayesOpt.jl",
-    modules = [AbstractBayesOpt],
-    checkdocs = :none,
-    pages = [
         "Home" => "index.md",
         "Public API" => "reference.md",
         "Tutorials" => [
@@ -38,8 +32,6 @@ makedocs(;
             "2D Bayesian Optimisation" => "tutorials/2D_BO.md",
         ]
     ],
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true"
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     ))
